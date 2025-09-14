1b8271591cb2062ab1be2ab3b4f7f74ed0bac182
tangtangyuhan <atang05@smith.edu>
2025-09-14 13:41:07 -0400

    Completed all code but format needs adjustment

--- computer.py
+++ computer.py
@@ -1,10 +1,29 @@
 class Computer:
-
-    # What attributes will it need?
+    description: str
+    processor_type: str
+    hard_drive_capacity: int
+    memory: int
+    operating_system: str
+    year_made: int
+    price: int
 
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    def __init__(self, description: str, processor_type: str, hard_drive_capacity: int, memory: int, operating_system: str, year_made: int, price: int):
+        self.description = description
+        self.processor_type = processor_type
+        self.hard_drive_capacity = hard_drive_capacity
+        self.memory = memory
+        self.operating_system = operating_system
+        self.year_made = year_made
+        self.price = price
+
+item_0 : Computer = Computer('2019 MacBook Pro', 'Intel', 256,  16, 'High Sierra', 2019, 1000)
+
+# def main():
+#     print(item_0.description)
+
+# if __name__ == "__main__":
+#     main()
 
     # What methods will you need?
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,10 +1,64 @@
+from computer import *
+from typing import Optional
+
 class ResaleShop:
 
-    # What attributes will it need?
+    def __init__(self):
+        self.inventory = [item_0]
+        print("Initially, the shop has:", item_0.description)
 
-    # How will you set up your constructor?
-    # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    def buy(self, computer: Computer):
+        self.inventory.append(computer)
+    
+    def sell(self, computer: Computer):
+        if computer in self.inventory:
+            self.inventory.remove(computer)
+    
+    def get_inventory(self):
+        for item in self.inventory:
+            print(f'Item ID: {self.inventory.index(item)} - [description:{item.description}], [processor type:{item.processor_type}], [hard drive capacity:{item.hard_drive_capacity}], [memory:{item.memory}], [operating system:{item.operating_system}], [year made:{item.year_made}], [price:{item.price}]')
 
-    # What methods will you need?
+    def update_price(self, new_price:int):
+        for item in self.inventory:
+            item.price = new_price
+
+    def refurbish(self, new_os: Optional[str] = None):
+        for computer  in self.inventory:
+            if int(computer.year_made) < 2000:
+                computer.price = 0 # too old to sell, donation only
+            elif int(computer.year_made) < 2012:
+                computer.price = 250 # heavily-discounted price on machines 10+ years old
+            elif int(computer.year_made) < 2018:
+                computer.price = 550 # discounted price on machines 4-to-10 year old machines
+            else:
+                computer.price = 1000 # recent stuff
+
+            if new_os is not None:
+                computer.operating_system = new_os
+        # else:
+        #     print("Item not found. Please select another item to refurbish.")
+
+
+item_1 = Computer("Mac Pro (Late 2013)", "3.5 GHc 6-Core Intel Xeon E5", 1024, 64, "macOS Big Sur", 2013, 1500)
+
+def main():
+    shop: ResaleShop = ResaleShop()
+
+    shop.buy(item_1)
+    print("After buying, the shop has:")
+    shop.get_inventory()
+
+    shop.sell(item_1)
+    print("After selling, the shop has:")
+    shop.get_inventory()
+
+    shop.update_price(300)
+    print("After updating price, the shop has:")
+    shop.get_inventory()
+
+    shop.refurbish("new-os")
+    print("After refurbishing, the shop has:")
+    shop.get_inventory()
+
+if __name__ == "__main__":
+    main()
